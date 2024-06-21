19a2fd8ab70e33b3ba867954b109ae0442c100e0
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2024-06-21 11:02:10 +0200

    remove unsafe block because lifetimes are specified now

--- src/lib.rs
+++ src/lib.rs
@@ -90,12 +90,8 @@ impl<'a> State<'a> {
             backends: wgpu::Backends::all(),
             ..Default::default()
         });
-
-        // # Safety
-        //
-        // The surface needs to live as long as the window that created it.
-        // State owns the window so this should be safe.
-        let surface = unsafe { instance.create_surface(window) }.unwrap();
+        
+        let surface = instance.create_surface(window).unwrap();
 
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
